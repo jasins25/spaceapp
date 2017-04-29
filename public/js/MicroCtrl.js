9210669500f2395779b6c40e986a087d5b42383e
jolyn91 <jolyn91@bitbucket.com>
2017-04-29 22:18:20 +0800

    Micro stuff, still ugly/buggy

--- public/js/MicroCtrl.js
+++ public/js/MicroCtrl.js
@@ -5,14 +5,53 @@
     function MicroCtrl($http, $scope) {
         var vm = this;
 
-        // vm.totalExpenditure = function () {
-        //     // console.log("vm.cartListArray: " + JSON.stringify(vm.cartListArray));
-        //     var totalArrayExpenditure = 0;
-        //     for (var i = 0; i < vm.expenditureListArray.length; i++) {
-        //         totalArrayExpenditure += vm.expenditureListArray[i].watts;
-        //     }
-        //     return totalArrayExpenditure;
-        // };
+        vm.energyExpenditure =[];
+        vm.wattsDuration = "";
+
+        vm.applianceArray = [
+            {
+                title: "Kettle",
+                watts: 200
+            },
+            {
+                title: "Air-conditioner",
+                watts: 250
+            },
+            {
+                title: "Laptop",
+                watts: 100
+            },
+            {
+                title: "Television",
+                watts: 222
+            }
+        ];
+
+        vm.addToExpenditure = function (applianceObj) {
+            vm.energyExpenditure.push(applianceObj);
+        };
+
+        vm.calcWatts = function (idx, applianceObj, duration) {
+            if (!duration) {
+                return 0;
+            }
+            vm.energyExpenditure[idx].specificWatts = applianceObj.watts * (duration/60);
+           return applianceObj.watts * (duration/60);
+        };
+
+        vm.totalExpenditure = function () {
+            var totalArrayExpenditure = 0;
+            for (var i = 0; i < vm.energyExpenditure.length; i++) {
+                totalArrayExpenditure += vm.energyExpenditure[i].specificWatts;
+                if (totalArrayExpenditure > vm.maxWatts) {
+                    vm.warning = "YOUR EXPENDITURE HAS EXCEEDED OMGZ";
+                }
+                else {
+                    vm.warning = "Your energy expenditure is doing fine!"
+                }
+            }
+            return totalArrayExpenditure;
+        };
 
     }
 
